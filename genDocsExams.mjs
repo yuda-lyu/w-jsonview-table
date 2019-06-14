74168333ee409f15875514b9ec25de7771e20117
yuda-lyu <firsemisphere@gmail.com>
2019-06-14 09:06:41 +0800

    add main

--- genDocsExams.mjs
+++ genDocsExams.mjs
@@ -23,7 +23,9 @@ async function main() {
     let cdn = `<script src="https://cdn.jsdelivr.net/npm/w-jsonview-table@${pkg.version}/dist/w-jsonview-table.umd.js"></script>`
 
     //mkdirSync
-    fs.mkdirSync(fd_tar)
+    if (!fs.existsSync(fd_tar)) {
+        fs.mkdirSync(fd_tar)
+    }
 
     //getFiles
     let ltfs = await getFiles(fd_src)
